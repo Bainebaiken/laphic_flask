8a1bc1c1a534660d99e9cab737b425118880039f
code-gi <mrmnamuddu@gmail.com>
2024-10-04 13:50:18 +0300

    initial deploy 2

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from invix_app import create_app
 from flask import Flask
 from flask_cors import CORS
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
 app = create_app()
 UPLOAD_FOLDER = 'uploads'
@@ -22,8 +22,12 @@ CORS(app)  # This enables CORS for all routes and all origins
 def articles():
     return {'articles': []}  # Example response
 
+# if __name__ == '__main__':
+#     app.run(debug=True, host='127.0.0.1', port=5000)
+
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    # Ensure the app runs on 0.0.0.0 if deploying on Render, not localhost
+    app.run(debug=True, host='0.0.0.0', port=5000)
 
 
 
